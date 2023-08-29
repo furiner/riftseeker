35fb92284b18e2106857ad6d6a0384d8f7649dbd
Irisu <nekomarisakirisame@gmail.com>
2023-08-29 07:33:43 -0400

    diddidididididdidi

--- src/main/java/net/arcadiasedge/riftseeker/utils/ColorMap.java
+++ src/main/java/net/arcadiasedge/riftseeker/utils/ColorMap.java
@@ -2,5 +2,5 @@ package net.arcadiasedge.riftseeker.utils;
 
 public class ColorMap {
     public static final String GRAY_1 = "#606060";
-    public static final String DARK_GRAY_1 = "#1f1f1f";
+    public static final String DARK_GRAY_1 = "#2e2e2e";
 }
